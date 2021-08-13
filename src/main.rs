5879e636b270d81cd15b1937b6416dcfc6c6f032
Surya Teja K <shanmukhateja@gmail.com>
2021-08-13 11:00:00 +0530

    Add IDE window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use gtk::prelude::*;
+
+fn build_ui(app: &gtk::Application) {
+    let window = gtk::ApplicationWindowBuilder::new()
+        .title("MyStudio IDE")
+        .default_width(800)
+        .default_height(600)
+        .application(app)
+        .border_width(10)
+        .visible(true)
+        .build();
+
+    window.show_all();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let application =
+        gtk::Application::new(Some("com.github.shanmukhateja.my-studio-ide"), Default::default());
+
+    application.connect_activate(build_ui);
+
+    application.run();
 }
