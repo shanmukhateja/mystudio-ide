087ecbcb42e1c34e930ad4c89029a301742e34cf
Surya Teja K <shanmukhateja@gmail.com>
2021-08-15 14:40:37 +0530

    feat: add small margin at start for text_view to improve text visibility

--- src/ui/text_view.rs
+++ src/ui/text_view.rs
@@ -2,6 +2,7 @@ pub fn build_text_view() -> gtk::TextView {
     let text_view = gtk::TextViewBuilder::new()
         .editable(true)
         .has_focus(true)
+        .margin_start(5)
         .hexpand(true)
         .build();
 
