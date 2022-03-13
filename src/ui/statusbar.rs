caf68526a5efac6641bb9d8cf914e8442167c480
Surya Teja K <shanmukhateja@gmail.com>
2022-03-13 13:46:22 +0530

    feat: hide statusbar message after 3 secs

--- src/ui/statusbar.rs
+++ src/ui/statusbar.rs
@@ -1,6 +1,11 @@
+use std::time::Duration;
+
 use gtk::traits::StatusbarExt;
 
 use crate::G_STATUS_BAR;
+
+const CONTENT_ID: u32 = 0;
+const MESSAGE_HIDE_DELAY: Duration = Duration::from_secs(3);
 
 fn get_status_bar() -> gtk::Statusbar {
     G_STATUS_BAR.with(|status_bar| {
@@ -15,12 +20,20 @@ fn get_status_bar() -> gtk::Statusbar {
 fn update_status_message(message: String) {
     let status_bar_ref = get_status_bar();
 
-    status_bar_ref.push(0, &message);
+    status_bar_ref.push(CONTENT_ID, &message);
+}
+
+fn reset_status_message() {
+    let status_bar_ref = get_status_bar();
+
+    status_bar_ref.remove_all(CONTENT_ID);
 }
 
 pub fn show_status_message(message: String) {
     // Show message
     update_status_message(message);
 
-    // TODO: hide message after few secs
+    gtk::glib::timeout_add_once::<_>(MESSAGE_HIDE_DELAY, || {
+        reset_status_message();      
+    });
 }
