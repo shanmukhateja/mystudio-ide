4c4dd763264dc872f3c6d4abeb02eda87267a211
Surya Teja K <shanmukhateja@gmail.com>
2022-06-05 12:40:06 +0530

    feat: move jump to line, fetch line number to lib

--- mystudio-ide/src/ui/statusbar/line_indicator.rs
+++ mystudio-ide/src/ui/statusbar/line_indicator.rs
@@ -14,7 +14,10 @@ use super::G_LINE_NUMBER;
 
 use crate::{ui::notebook::editor, G_BUILDER};
 
-use libmystudio::workspace::Workspace;
+use libmystudio::{
+    notebook::editor::{fetch_line_number_by_buffer, jump_to_line_with_editor},
+    workspace::Workspace,
+};
 
 pub(super) fn init() {
     G_BUILDER.with(|builder| {
@@ -136,31 +139,6 @@ fn jump_to_line(line: i32, col: i32) {
 
     let editor = editor.unwrap();
     jump_to_line_with_editor(&editor, line, col);
-}
-
-fn jump_to_line_with_editor(editor: &View, line: i32, col: i32) {
-    let buffer = editor.buffer().unwrap();
-
-    // We decrement line, col here as
-    // it is user input and buffer starts at 0
-    let mut iter = buffer.iter_at_line(line - 1);
-    iter.set_line_index(col - 1);
-
-    buffer.place_cursor(&iter);
-    // Set focus to editor
-    editor.set_is_focus(true);
-    editor.set_has_focus(true);
-}
-
-fn fetch_line_number_by_buffer(buffer: &Buffer) -> (i32, i32) {
-    let cursor_position = buffer.cursor_position();
-
-    let iter = buffer.iter_at_offset(cursor_position);
-
-    let line = iter.line();
-    let col = iter.line_index();
-
-    (line, col)
 }
 
 pub fn update(mut line: i32, mut col: i32, should_increment: bool) {
