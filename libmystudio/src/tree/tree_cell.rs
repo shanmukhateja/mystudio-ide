09dc243377cdad3f785eb9d3735b05e575a23ba8
Surya Teja K <shanmukhateja@gmail.com>
2022-07-10 11:56:00 +0530

    feat: add unit tests for get_icon_name

--- libmystudio/src/tree/tree_cell.rs
+++ libmystudio/src/tree/tree_cell.rs
@@ -61,10 +61,48 @@ fn get_icon_name(filename: &str) -> String {
 
 #[cfg(target_os = "windows")]
 fn get_icon_name(filename: &str) -> String {
-
     new_mime_guess::from_path(filename)
         .first_or_text_plain()
         .to_string()
         // FIXME: find a better way
         .replace('/', "-")
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::tree::tree_cell::get_icon_for_name;
+
+    fn test_icon_name_mime<'a>(name: &'a str, mime: &'a str, fallback_mime: Option<&'a str>) -> bool {
+        let fetched_mime = get_icon_for_name(name, crate::tree::tree_model::TreeNodeType::File);
+
+        if let Some(fallback) = fallback_mime {
+            fetched_mime == fallback || fetched_mime == mime
+        } else {
+            fetched_mime == mime
+        }
+    }
+
+    /**
+     * Note: This test needs to be run on Linux and Windows for 100% assurance.
+     */
+    #[test]
+    fn get_icon_name_rust() {
+        assert!(test_icon_name_mime(
+            "file.rs",
+            "text-rust",
+            Some("text-x-rust")
+        ));
+    }
+
+    /**
+     * This test needs to be run on Linux and Windows for 100% assurance.
+     */
+    #[test]
+    fn get_icon_name_js() {
+        assert!(test_icon_name_mime(
+            "file.js",
+            "application-javascript",
+            None
+        ));
+    }
 }
